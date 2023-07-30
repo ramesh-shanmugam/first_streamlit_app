6d3127f703551753183af841e8409d476d4f8b8f
ramesh-shanmugam <58585986+ramesh-shanmugam@users.noreply.github.com>
2023-07-30 12:23:50 +0530

    Update streamlit_app.py
    added user input option

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,15 @@ streamlit.dataframe(fruits_to_show)
 
 # to get data from Fruitvice api
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+#streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
