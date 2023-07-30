9d8233228ab2ff3b3aeba837dc663715d1907fd6
ramesh-shanmugam <58585986+ramesh-shanmugam@users.noreply.github.com>
2023-07-30 18:22:09 +0530

    Update streamlit_app.py
    fixing dataframe read error

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,6 @@ def get_fruitvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-
 # to get data from Fruitvice api
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -41,7 +40,6 @@ try:
     back_from_function = get_fruitvice_data(fruit_choice)
     # write your own comment - what does this do?
     streamlit.dataframe(back_from_function)
-
 except:
   streamlit.error()
 
@@ -60,7 +58,7 @@ streamlit.dataframe(my_data_rows)
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    return  "Thanks for adding new fruit"+new_fruit
+    return  "Thanks for adding  "+new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button("Add a Fruit to the list"):
